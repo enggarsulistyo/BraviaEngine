90bf7e11fc5882797df9ebeb72a31a4b5d6b49ba
Enggar Sulistyo <enggar.sulistyo@gmail.com>
2018-11-30 21:28:38 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -88,7 +88,7 @@ set_permissions() {
   # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
 
   # The following is default permissions, DO NOT remove
-   set_perm_recursive  $MODPATH/system/bin  0  0   0755    0755
+   set_perm_recursive  $MODPATH/system/bin  0  2000   0755    0755
    set_perm_recursive  $MODPATH/system/app  0  0   0755    0644
    chmod 0755 $MODPATH/system/etc/be_album
    chmod 0755 $MODPATH/system/etc/be_photo
