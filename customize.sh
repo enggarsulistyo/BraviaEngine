c9191cd351bb87dd8f4ab7e4006511ad1aba0e88
Enggar Sulistyo <enggar.sulistyo@gmail.com>
2023-10-21 13:00:49 +0700

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -134,7 +134,7 @@ on_install() {
   # The following is the default implementation: extract $ZIPFILE/system to $MODPATH
   # Extend/change the logic to whatever you want
   ui_print "- Extracting module files"
-  unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+  unzip -o "$ZIPFILE" 'system' -d $MODPATH >&2
 }
 
 # Only some special files require specific permissions
